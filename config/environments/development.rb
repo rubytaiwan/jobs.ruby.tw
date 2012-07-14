a8422f7d0815de4711ad53ad7adaacde2e215dc1
xdite <xdite@rocodev.com>
2012-07-15 04:37:38 +0800

    fix mail problem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ JobsRubyTw::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } 
 end
