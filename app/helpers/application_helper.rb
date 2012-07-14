9d868141fd39c4d3959ffaaa44a143fa1deaf998
xdite <xdite@rocodev.com>
2012-07-15 04:37:30 +0800

    refactor to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def render_notice
+    if flash[:notice]
+      content_tag(:div,  flash[:notice], :class => "message notice")
+    end
+  end
+
+  def render_warning
+    if flash[:warning]
+      content_tag(:div,  flash[:alert], :class => "message alert")
+    end
+  end
 end
