7793a7ee66ca03676fb0da22e60096f4c37f3517
xdite <xdite@rocodev.com>
2012-07-15 04:28:46 +0800

    Add precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ JobsRubyTw::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.assets.precompile += %w( application.css application.js mobile.css mobile.js )
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
