2be8494272e2f161e34b6b8a5e419225012cac32
xdite <xdite@rocodev.com>
2012-07-15 04:06:26 +0800

    upgrade to 3.2.6

--- Gemfile
+++ Gemfile
@@ -1,23 +1,28 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.9'
+gem 'rails', '3.2.6'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.0'
+  gem 'coffee-rails', '~> 3.2.0'
+end
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'devise', '1.1.3'
+gem 'devise', '~>1.5'
 gem 'mysql2', '~>0.2.7'
 
 gem "babosa"
 gem 'exception_notification'
 
-gem 'capistrano'
+gem 'capistrano', :group => "development"
 
 group :test, :development do
   gem 'sqlite3'
   gem "rspec", "~> 2.0"
   gem "rspec-rails", "~> 2.0"
-  gem "rcov"
+  gem "simplecov"
   gem "watchr"
   gem "capybara"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -23,8 +24,8 @@ ActiveRecord::Schema.define(:version => 20100715071050) do
     t.text     "apply_information"
     t.date     "deadline"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
     t.string   "aasm_state"
   end
 
@@ -33,7 +34,6 @@ ActiveRecord::Schema.define(:version => 20100715071050) do
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(:version => 20100715071050) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
