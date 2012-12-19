4eda472334592cbbf35b8ebd1dd103f149c9c300
xdite <xdite@rocodev.com>
2012-12-19 23:27:42 +0800

    add some useful gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   has_mobile_fu
-  
+
   protect_from_forgery
-  
+
   before_filter :set_locale
 
   rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
-  
+
   protected
-  
+
   def set_locale
     if params[:locale] && ["en", "zh_tw"].include?( params[:locale] )
       session[:locale] = params[:locale]
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
     I18n.locale = session[:locale] || I18n.default_locale
   end
-  
+
   def render_not_found
     render template: "pages/404", status: 404
   end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,8 @@
 class JobsController < ApplicationController
-  
+
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_filter :find_my_job, only: [:edit, :update, :destroy, :open, :close]
-  
+
   def index
     if params[:user_id]
       @jobs = User.find(params[:user_id]).jobs.recent
@@ -12,26 +12,26 @@ class JobsController < ApplicationController
       @jobs = Job.online.recent
     end
   end
-  
+
   def show
     @job = Job.find(params[:id])
   end
-  
+
   def new
     @job = current_user.jobs.build
     @job.deadline = Time.zone.now + 90.days
   end
-  
+
   def create
     @job = current_user.jobs.build(params[:job])
-    
+
     if @job.save
       redirect_to job_path(@job)
     else
       render :new
     end
   end
-  
+
   def edit
   end
 
@@ -42,39 +42,39 @@ class JobsController < ApplicationController
 
     render layout: false
   end
-  
-  def update    
+
+  def update
     if @job.update_attributes(params[:job])
       redirect_to job_path(@job)
     else
       render :edit
-    end    
+    end
   end
-  
+
   def destroy
     @job.destroy
-        
+
     redirect_to jobs_path
   end
-  
+
   def open
     @job.open
     @job.save!
-    
+
     redirect_to job_path(@job)
   end
-  
+
   def close
     @job.close
     @job.save!
-    
-    redirect_to job_path(@job)    
+
+    redirect_to job_path(@job)
   end
 
   protected
-  
+
   def find_my_job
     @job = current_user.jobs.find(params[:id])
   end
-    
+
 end
