60fd9eeb96931a3c19bbbc993751c851b12e1852
Juanito Fatas <katehuang0320@gmail.com>
2013-11-19 01:26:10 +0800

    update_attribute is deprecated, use update instead.

--- spec/features/job_spec.rb
+++ spec/features/job_spec.rb
@@ -12,7 +12,7 @@ feature 'View individual job pages' do
   end
 
   scenario 'as a guest and somehow job loses its owner' do
-    @job.update_attribute(:owner, nil)
+    @job.update(owner: nil)
     visit job_path(@job)
     expect(page).to_not have_css('span.manage')
   end
