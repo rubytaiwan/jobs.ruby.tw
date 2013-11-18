5b5e60aff2c0dadf7b99c23fa5f2f67c853c929b
Juanito Fatas <katehuang0320@gmail.com>
2013-11-19 02:03:45 +0800

    Add DatabaseRewinder and update some configs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
-# -*- encoding : utf-8 -*-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -26,4 +26,28 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+
+  config.before(:suite) do
+    DatabaseRewinder.strategy = :truncation
+    DatabaseRewinder.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseRewinder.start
+  end
+
+  config.after(:each) do
+    DatabaseRewinder.clean
+  end
 end
