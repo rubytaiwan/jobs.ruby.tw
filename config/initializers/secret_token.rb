852b4851794d9b607c81ee99d27ec9c76383cf89
Juanito Fatas <katehuang0320@gmail.com>
2013-11-19 00:59:56 +0800

    Add secret_key_base.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,3 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 JobsRubyTw::Application.config.secret_token = 'ddda33dbe4d87cfecc1958a081ebe06075c14e679fbdfd2ee15ad3242645c793112a3fa35b33def08d29afabf9eb3915fdede91cec11d54006c340b94dd850e9'
+JobsRubyTw::Application.config.secret_key_base = 'ce9acd6674c09982ed5f374ac2f408f2d9303a8859066f7dc45ada53362106ebf566b31759addf3486502bc7be0386eead1546d8522c099f7ec36074530e7493'
