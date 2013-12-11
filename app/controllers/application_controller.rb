cd99a767096957e37c585a88221376cb6625adc2
Juanito Fatas <katehuang0320@gmail.com>
2013-12-11 22:51:33 +0800

    Use http accept language to handle locale.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   has_mobile_fu
 
-  protect_from_forgery
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
 
   before_action :set_locale
 
@@ -10,14 +12,16 @@ class ApplicationController < ActionController::Base
   protected
 
   def set_locale
-    if params[:locale] && ["en", "zh_tw"].include?( params[:locale] )
-      session[:locale] = params[:locale]
-    end
+    session[:locale] = params[:locale] if user_set_available_locale?
+    I18n.locale = session[:locale] || http_accept_language.compatible_language_from(I18n.available_locales)
+  end
 
-    I18n.locale = session[:locale] || I18n.default_locale
+  def user_set_available_locale?
+    params[:locale] && params[:locale].in?(I18n.available_locales.map(&:to_s))
   end
 
   def render_not_found
     render template: "pages/404", status: 404
   end
 end
+
