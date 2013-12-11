5851d630d42c63f025c4b3ff8b4eddaafa776063
Juanito Fatas <katehuang0320@gmail.com>
2013-12-11 23:27:00 +0800

    Ransack to search job title, clean some code.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,13 +1,21 @@
 class JobsController < ApplicationController
-
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_my_job, only: [:edit, :update, :destroy, :open, :close]
 
+  # FIXME:
+  # 現在 search 綁在 application.html.erb 裡，所以每個 action 都需要 @q，
+  # 不然會噴：
+  #   ArgumentError at /jobs/4-mad-rubyist-apple-inc
+  #   No Ransack::Search object was provided to search_form_for!
+  # 應該要把 view 拆開，目前暫時只能搜索 title。
+  before_action :get_search_object
+
   def index
+    @q = Job.search(params[:q])
     @jobs = if params[:user_id]
               User.find(params[:user_id]).jobs.recent
-            elsif params[:keyword]
-              Job.online.search(params[:keyword])
+            elsif params[:q]
+              @q.result(distinct: true)
             else
               Job.online.recent
             end
@@ -82,7 +90,14 @@ class JobsController < ApplicationController
 
   private
 
+  def get_search_object
+    @q = Job.search(params[:q])
+  end
+
   def job_params
-    params.require(:job).permit(:title, :job_type, :occupation, :company_name, :location, :url, :description, :apply_information, :deadline)
+    params.require(:job)
+      .permit(:title, :job_type, :occupation, :company_name,
+              :location, :url, :description, :apply_information,
+              :deadline)
   end
 end
