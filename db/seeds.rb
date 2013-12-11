479fb8f51cc2447ae67f49f009881523da5ebe64
Juanito Fatas <katehuang0320@gmail.com>
2013-12-11 22:47:36 +0800

    Embrace Ruby 1.9 syntax.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,16 @@
-# -*- encoding : utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Daley', city: cities.first)
 
-user = User.create!(:email => "admin@ruby.tw", :password => "rubytw", :password_confirmation => "rubytw")
+user = User.create!(email: "admin@ruby.tw", password: "rubytw", password_confirmation: "rubytw")
 user.confirmed_at = Time.now
 user.save
 
-job = Job.create!( :title => "Rails developer", :url => "http://ruby.tw",
-                   :company_name => "Ruby Taiwan", :job_type => "Full-time", :occupation => "Web back-end",
-                   :location => "Taipei", :user_id => 1,
-                   :description => "This is awesome Ruby job!<br>The salary is also great!", :apply_information => "Please email to me" )
+job = Job.create!( title: "Rails developer", url: "http://ruby.tw",
+                   company_name: "Ruby Taiwan", job_type: "Full-time", occupation: "Web back-end",
+                   location: "Taipei", user_id: 1,
+                   description: "This is awesome Ruby job!<br>The salary is also great!", apply_information: "Please email to me" )
