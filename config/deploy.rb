0761c0dcd7389bb72e6453e877032d2813567a88
Awaru Ryudo <ryudoawaru@gmail.com>
2015-02-21 17:53:11 +0800

    modified:   config/deploy.rb 	modified:   config/deploy/production.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,6 @@ set :scm, :git
 set :linked_files, %w{config/database.yml config/email.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :default_env, { path: "/usr/local/ruby22/bin:$PATH" }
 set :keep_releases, 3
 
 after :'deploy:publishing', :'deploy:restart'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+set :default_env, { path: "/usr/local/ruby21/bin:$PATH" }
 set :deploy_to, '/home/deploy/jobs.ruby.tw'
 set :branch, ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
 set :stage, :production
