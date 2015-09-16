78929f443ee70e5d99de369337dfbf96bfa41543
Juanito Fatas <katehuang0320@gmail.com>
2015-09-16 17:19:40 +0800

    Make specs pass and warnings free

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe JobsController, type: :controller do
 
@@ -14,145 +14,146 @@ describe JobsController, type: :controller do
 
   describe 'GET index' do
     it 'should render successful' do
-      Job.stub_chain(:online, :recent).and_return([@job])
+      expect(Job).to receive(:online).and_return(double("job", recent: [@job]))
       get :index
 
-      assigns[:jobs].should eq [@job]
-      response.should be_success
+      expect(assigns[:jobs]).to eq [@job]
+      expect(response).to be_success
     end
 
     context 'by keyword' do
       it 'should render successful' do
         online_jobs = double('jobs')
-        Job.should_receive(:online).and_return(online_jobs)
-        online_jobs.should_receive('search').with('rails').and_return([@job])
+        expect(Job).to receive(:online).and_return(online_jobs)
+        expect(online_jobs).to receive('search').with('rails').and_return([@job])
 
         get :index, keyword: 'rails'
 
-        assigns[:jobs].should eq [@job]
-        response.should be_success
+        expect(assigns[:jobs]).to eq [@job]
+        expect(response).to be_success
       end
     end
 
     context 'by user' do
       it 'should render successful' do
         user = double('user')
-        User.should_receive(:find).with('99').and_return(user)
-        user.stub_chain(:jobs, :recent).and_return([@job])
+        expect(User).to receive(:find).with('99').and_return(user)
+        expect(user).to receive(:jobs).and_return(user)
+        expect(user).to receive(:recent).and_return([@job])
 
         get :index, user_id: 99
 
-        assigns[:jobs].should eq [@job]
-        response.should be_success
+        expect(assigns[:jobs]).to eq [@job]
+        expect(response).to be_success
       end
     end
   end
 
   describe 'GET show' do
     it 'should render successful' do
-      Job.should_receive(:find).with('123').and_return(@job)
+      expect(Job).to receive(:find).with('123').and_return(@job)
       get :show, id: 123
 
-      assigns[:job].should eq @job
-      response.should be_success
+      expect(assigns[:job]).to eq @job
+        expect(response).to be_success
     end
   end
 
   describe 'GET new' do
     it 'should render successful' do
-      Job.should_receive(:new).and_return(@job)
+      expect(Job).to receive(:new).and_return(@job)
       get :new
 
-      assigns[:job].should eq @job
-      response.should be_success
+      expect(assigns[:job]).to eq @job
+      expect(response).to be_success
     end
   end
 
   describe 'POST create' do
     it 'should save successful and redirect to show' do
-      current_user.jobs.should_receive(:build).with('title' => 'abc').and_return(@job)
-      @job.should_receive(:save).and_return(true)
+      expect(current_user.jobs).to receive(:build).with('title' => 'abc').and_return(@job)
+      expect(@job).to receive(:save).and_return(true)
       post :create, job: { title: 'abc' }
 
-      response.should be_redirect
+      expect(response).to be_redirect
     end
 
     it 'should save failed and render new' do
       post :create, job: { titie: '' }
 
-      response.should render_template(:new)
-      response.should be_success
+      expect(response).to render_template(:new)
+      expect(response).to be_success
     end
   end
 
   describe 'GET edit' do
     it 'should render successful' do
-      current_user.stub_chain(:jobs, :find).with('123').and_return(@job)
+      expect(current_user.jobs).to receive(:find).with('123').and_return(@job)
       get :edit, id: 123
 
-      assigns[:job].should eq @job
-      response.should be_success
+      expect(assigns[:job]).to eq @job
+      expect(response).to be_success
     end
   end
 
   describe 'PUT update' do
     it 'should save successful and redirect to show' do
-      current_user.stub_chain(:jobs, :find).with('123').and_return(@job)
-      @job.should_receive(:update_attributes).with('title' => 'abc').and_return(true)
+      expect(current_user.jobs).to receive(:find).with('123').and_return(@job)
+      expect(@job).to receive(:update_attributes).with('title' => 'abc').and_return(true)
       put :update, id: 123, job: { title: 'abc' }
 
-      response.should be_redirect
+      expect(response).to be_redirect
     end
 
     it 'should save failed and render new' do
-      current_user.stub_chain(:jobs, :find).with('123').and_return(@job)
+      expect(current_user.jobs).to receive(:find).with('123').and_return(@job)
       put :update, id: 123, job: { title: '' }
 
-      response.should render_template(:edit)
-      response.should be_success
+      expect(response).to render_template(:edit)
+      expect(response).to be_success
     end
   end
 
   describe 'DELETE destroy' do
     it 'should delete successful' do
-      current_user.stub_chain(:jobs, :find).with('123').and_return(@job)
-      @job.should_receive(:destroy)
+      expect(current_user.jobs).to receive(:find).with('123').and_return(@job)
+      expect(@job).to receive(:destroy)
       delete :destroy, id: 123
-      response.should be_redirect
+      expect(response).to be_redirect
     end
 
   end
 
   describe 'GET/POST preview' do
     it 'should render successful' do
-      current_user.jobs.should_receive(:build).with('title' => 'abc').and_return(@job)
-      @job.should_receive(:valid?)
+      expect(current_user.jobs).to receive(:build).with('title' => 'abc').and_return(@job)
+      expect(@job).to receive(:valid?)
 
       post :preview, job: { title: 'abc' }
 
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe 'PUT open' do
     it 'should open job' do
-      current_user.stub_chain(:jobs, :find).with('123').and_return(@job)
-      @job.should_receive(:open)
-      @job.should_receive(:save!)
+      expect(current_user.jobs).to receive(:find).with('123').and_return(@job)
+      expect(@job).to receive(:open)
+      expect(@job).to receive(:save!)
 
       put :open, id: 123
-      response.should be_redirect
+      expect(response).to be_redirect
     end
   end
 
   describe 'PUT close' do
     it 'should open job' do
-      current_user.stub_chain(:jobs, :find).with('123').and_return(@job)
-      @job.should_receive(:close)
-      @job.should_receive(:save!)
+      expect(current_user.jobs).to receive(:find).with('123').and_return(@job)
+      expect(@job).to receive(:close)
+      expect(@job).to receive(:save!)
 
       put :close, id: 123
-      response.should be_redirect
+      expect(response).to be_redirect
     end
   end
 
--- spec/features/job_spec.rb
+++ spec/features/job_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 feature 'View individual job pages' do
   background do
