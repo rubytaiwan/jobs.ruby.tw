4c4c776b64d2fc4ab3564d10bbb7fb99353766c7
Juanito Fatas <katehuang0320@gmail.com>
2015-09-16 17:19:10 +0800

    Update RSpec config files

---
+++ spec/support/factory_helper.rb
@@ -0,0 +1,16 @@
+module FactoryHelper
+  def build_user(attributes = {})
+    user = User.new({ email: 'admin@ruby.tw', password: 'rubytw8888', password_confirmation: 'rubytw8888' }.merge(attributes))
+    user.skip_confirmation!
+    user.save!
+    user
+  end
+
+  def build_job(attributes = {})
+    attributes[:owner] ||= build_user
+    Job.create!({ title: 'Rails developer', url: 'http://ruby.tw',
+                  company_name: 'Ruby Taiwan', job_type: 'Full-time', occupation: 'Web back-end',
+                  location: 'Taipei', user_id: 1, deadline: Date.parse('2099-06-28'),
+                  description: 'This is awesome Ruby job!<br>The salary is also great!', apply_information: 'Please email to me' }.merge(attributes))
+  end
+end
--- spec/support/utils.rb
+++
@@ -1,8 +0,0 @@
-def login!
-  visit '/users/sign_in'
-  within('#new_user') do
-    fill_in 'Email', with: @user.email
-    fill_in 'Password', with: 'rubytw8888'
-  end
-  click_button 'Sign in'
-end
