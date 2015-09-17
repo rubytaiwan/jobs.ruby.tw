0111a18e8a14b2016e35fcd8348e8c599ed0369d
Juanito Fatas <katehuang0320@gmail.com>
2015-09-17 12:47:11 +0800

    Remove annotates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :integer          not null, primary key
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0)
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  confirmation_token     :string(255)
-#  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  reset_password_sent_at :datetime
-#
-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
