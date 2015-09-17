e782245094f32cb3718e043f29c9e2e203fff475
Awaru Ryudo <ryudoawaru@gmail.com>
2015-09-17 11:52:03 +0800

    Change production deploy env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ set :default_env, { path: "/usr/local/ruby22/bin:$PATH" }
 set :deploy_to, '/home/rubytw/jobs.ruby.tw'
 set :branch, ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
 set :stage, :production
-role :web, "deploy@5xruby.tw"
-role :app, "deploy@5xruby.tw"
-role :db,  "deploy@5xruby.tw", :primary => true
+role :web, "rubytw@60.250.121.194"
+role :app, "rubytw@60.250.121.194"
+role :db,  "rubytw@60.250.121.194", :primary => true
